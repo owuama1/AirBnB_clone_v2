9959a05b0fd9de4c4c9f6b345597515ffdc97022
owuama1 <kelechiowuama24@gmail.com>
2024-06-12 10:29:06 +0000

    update user.py

--- models/user.py
+++ models/user.py
@@ -1,31 +1,34 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+import os
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models import storage_type
 from models.base_model import BaseModel, Base
-from models.__init__ import storage_type  # Import storage_type
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-
     __tablename__ = 'users'
-
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-
-    if storage_type == 'db':
-        places = relationship("Place", backref="user",
-                              cascade="all, delete-orphan")
-    else:
-        @property
-        def places(self):
-            """Getter attribute places that returns the list of Place instances
-            with user_id equals to the current User.id"""
-            from models import storage
-            all_places = storage.all(Place)
-            user_places = [place for place in all_places.values()
-                           if place.user_id == self.id]
-            return user_places
+    email = Column(
+        String(128), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    password = Column(
+        String(128), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    first_name = Column(
+        String(128), nullable=True
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    last_name = Column(
+        String(128), nullable=True
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    places = relationship(
+        'Place',
+        cascade="all, delete, delete-orphan",
+        backref='user'
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
+    reviews = relationship(
+        'Review',
+        cascade="all, delete, delete-orphan",
+        backref='user'
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
