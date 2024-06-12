a1912163c54aeb763748e074fa06d7910d4edc5c
owuama1 <kelechiowuama24@gmail.com>
2024-06-12 10:48:22 +0000

    update user.py, review.py and place.py

--- models/place.py
+++ models/place.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from sqlalchemy import Column, String, Integer, Float
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
+from os import getenv
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
 
     __tablename__ = 'places'
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        reviews = relationship("Review", cascade="all, delete", backref="place")
+    else:
+        @property
+        def reviews(self):
+            """ Getter attribute that returns the list of Review instances """
+            from models import storage
+            review_list = []
+            for review in storage.all(Review).values():
+                if review.place_id == self.id:
+                    review_list.append(review)
+            return review_list
 
     city_id = Column(String(60), nullable=False)
     user_id = Column(String(60), nullable=False)
