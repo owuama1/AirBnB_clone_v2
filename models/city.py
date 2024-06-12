0513e55dbe4ca6bca9ac0085fdb915d1110e076c
owuama1 <kelechiowuama24@gmail.com>
2024-06-12 10:04:02 +0000

    update place.py, user.py and city.py

--- models/city.py
+++ models/city.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 
 
@@ -10,3 +11,17 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
+    if storage_type == 'db':
+        places = relationship("Place", backref="city",
+                              cascade="all, delete-orphan")
+    else:
+        @property
+        def places(self):
+            """Getter attribute places that returns the list of Place instances
+            with city_id equals to the current City.id"""
+            from models import storage
+            all_places = storage.all(Place)
+            city_places = [place for place in all_places.values()
+                           if place.city_id == self.id]
+            return city_places
--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 
 
@@ -13,3 +14,17 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+
+    if storage_type == 'db':
+        places = relationship("Place", backref="user",
+                              cascade="all, delete-orphan")
+    else:
+        @property
+        def places(self):
+            """Getter attribute places that returns the list of Place instances
+            with user_id equals to the current User.id"""
+            from models import storage
+            all_places = storage.all(Place)
+            user_places = [place for place in all_places.values()
+                           if place.user_id == self.id]
+            return user_places
