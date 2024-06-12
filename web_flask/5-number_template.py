fad742f116a703f787417da591e45051054c457f
owuama1 <kelechiowuama24@gmail.com>
2024-06-12 14:09:35 +0000

    edit

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -10,80 +10,38 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """
-    Display 'Hello HBNB!'
-
-    Returns:
-        str: The message 'Hello HBNB!'
-    """
+    """Display 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """
-    Display 'HBNB'
-
-    Returns:
-        str: The message 'HBNB'
-    """
+    """Display 'HBNB'"""
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    """
-    Display 'C ' followed by the value of the text variable
-
-    Args:
-        text (str): The text to display
-
-    Returns:
-        str: The message 'C ' followed by the value of the text variable
-    """
+    """Display 'C ' followed by the value of the text variable"""
     return "C {}".format(text.replace('_', ' '))
 
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text="is cool"):
-    """
-    Display 'Python ' followed by the value of the text variable
-
-    Args:
-        text (str): The text to display (default is 'is cool')
-
-    Returns:
-        str: The message 'Python ' followed by the value of the text variable
-    """
+    """Display 'Python ' followed by the value of the text variable"""
     return "Python {}".format(text.replace('_', ' '))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    """
-    Display 'n is a number' only if n is an integer
-
-    Args:
-        n (int): The number to display
-
-    Returns:
-        str: The message 'n is a number' if n is an integer
-    """
+    """Display 'n is a number' only if n is an integer"""
     return "{} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    """
-    Display a HTML page only if n is an integer
-
-    Args:
-        n (int): The number to display
-
-    Returns:
-        rendered_template: HTML template displaying the number
-    """
+    """Display a HTML page only if n is an integer"""
     return render_template('number.html', n=n)
 
 
